ba5e7fde830b1c65cc715ab6ef46f63959ec970b
Matheus Menegat <matheusmenegat@hotmail.com>
2023-01-06 18:50:17 -0300

    initial project structure config

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
 import React from 'react';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Home, About } from './pages';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+  </Routes>
+);
 
 export default App;
