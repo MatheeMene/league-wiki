5fdaaa252af9c026796b40e699f7c3f262b45752
felipezarichta <fezarichta@hotmail.com>
2023-01-10 22:54:45 -0400

    create about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import './styles.scss';
 
-export const About = () => (
-  <>
-    About
-  </>
+export const About: React.FC = () => (
+
+  <main>
+    <article className="about">
+      <h2>
+        Sobre nós
+        {' '}
+        <br />
+        Desenvolvedores
+      </h2>
+      <h3>Nossa história</h3>
+      <p>
+        Somos seres pertencentes ao grupo garuka pornstars, que buscam um conhecimento
+        na programção other level, com iniciativa do teacher matheuxx o malvadao
+      </p>
+    </article>
+    <section className="cards" />
+    <div className="card__1">
+      <p>Matheus Menegat</p>
+    </div>
+    <div className="card__2">
+      <p>Felipe Rodighero</p>
+    </div>
+    <div className="card__3">
+      <p>Anderson Severo</p>
+    </div>
+  </main>
+
 );
